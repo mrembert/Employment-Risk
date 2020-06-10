5af5c44f10043a0bfe8b396132182b98d34e7503
Mark Rembert <mrembert@gmail.com>
2020-06-10 09:32:42 -0400

    Adding population

--- monthly BLS data.R
+++ monthly BLS data.R
@@ -20,6 +20,17 @@ prop <- tbl(coririsi, in_schema("sch_layer", "bea_caemp25n_county_01_18")) %>%
   mutate(dprop07_18 = (prop_emp_2018 - prop_emp_2007)/prop_emp_2007) %>%
   mutate(fips = as.numeric(geoid)) %>%
   select(fips,dprop07_18, prop_emp_2007, prop_emp_2018) 
+
+pop <- tbl(coririsi, in_schema("sch_layer", "bea_cainc4_county_69_18")) %>% 
+  collect() %>% 
+  select(geoid,year, pop) %>% 
+  filter(year == 2007 | year == 2018) %>%
+  gather("var", "pop", pop) %>%
+  unite(temp, var, year) %>%
+  spread(temp, pop) %>%
+  mutate(dpop07_18 = (pop_2018 - pop_2007)/pop_2007) %>%
+  mutate(fips = as.numeric(geoid)) %>%
+  select(fips, dpop07_18, pop_2007, pop_2018) 
  
 
 broadband <- tbl(coririsi, in_schema("sch_analysis", "la_counties_broadband")) %>% 
@@ -85,7 +96,8 @@ df_emp <- df_emp %>% left_join(metro, "fips")%>%
   left_join(young_firm, "fips") %>%
   left_join(amenity, "fips") %>%
   left_join(black2010, "fips") %>%
-  left_join(hispanic2010, "fips")
+  left_join(hispanic2010, "fips") %>%
+  left_join(pop, "fips")
 
 df_emp <- df_emp %>% left_join(emp.2007, "fips") %>% 
   mutate(demp.2007.2019 = (emp.2019 - emp.2007)/emp.2007)
@@ -95,6 +107,9 @@ df_emp <- df_emp %>%
 
 df_emp$recovered <- 0
 df_emp$recovered[df_emp$demp.2007.2019>=0] <- 1 
+
+df_emp$high_black <- 0
+df_emp$high_black[df_emp$black2010>12.5] <-1
 
 df_analysis <- df_emp %>% filter(!is.na(amenity), !is.na(bach2018), !is.na(bach2010), !is.na(emp.2007), !is.na(emp.Apr2020), !is.na(broadband))
 
@@ -106,6 +121,7 @@ recovery_compare <- df_analysis %>%
     recession = mean(demp.2007.2019),
     covid = mean(demp.covid),
     recession.covid = mean(demp.2007.Apr20),
+    pop = mean(dpop07_18),
     black2010 = mean(black2010),
     hispanic2010 = mean(hispanic2010),
     bach2010 = mean(bach2010_share),
@@ -125,6 +141,7 @@ recovery_compare_nometro <- df_analysis %>%
     recession = mean(demp.2007.2019),
     covid = mean(demp.covid),
     recession.covid = mean(demp.2007.Apr20),
+    pop = mean(dpop07_18),
     black2010 = mean(black2010),
     hispanic2010 = mean(hispanic2010),
     bach2010 = mean(bach2010_share),
@@ -175,8 +192,52 @@ recovery_compare_alt_nometro <- df_analysis %>%
     amenity = mean(amenity)
   )
 
+recovery_compare_black <- df_analysis %>%
+  group_by(high_black) %>%
+  summarize(
+    n = n(),
+    recession = (sum(emp.2019) - sum(emp.2007))/sum(emp.2007),
+    covid = (sum(emp.Apr2020) - sum(emp.Feb2020))/sum(emp.Feb2020),
+    recession.covid = (sum(emp.Apr2020) - sum(emp.2007))/sum(emp.2007),
+    pop = mean(dpop07_18),
+    black2010 = mean(black2010),
+    hispanic2010 = mean(hispanic2010),
+    bach2010 = mean(bach2010_share),
+    bach2018 = mean(bach2018_share),
+    dbach = (sum(bach2018) - sum(bach2010))/sum(bach2010),
+    broadband_sub = mean(broadband_sub),
+    broadband = sum(f477_maxad_downup_2018dec_25_3_popsum)/sum(pop2018_sum),
+    young_firm = sum(firm_10yr)/sum(lodes_all_jobs_2017),
+    prop = (sum(prop_emp_2018)-sum(prop_emp_2007))/sum(prop_emp_2007),
+    amenity = mean(amenity)
+  )
+
+recovery_compare_black_metro <- df_analysis %>%
+  group_by(high_black, metro) %>%
+  summarize(
+    n = n(),
+    recession = (sum(emp.2019) - sum(emp.2007))/sum(emp.2007),
+    covid = (sum(emp.Apr2020) - sum(emp.Feb2020))/sum(emp.Feb2020),
+    recession.covid = (sum(emp.Apr2020) - sum(emp.2007))/sum(emp.2007),
+    pop = mean(dpop07_18),
+    black2010 = mean(black2010),
+    hispanic2010 = mean(hispanic2010),
+    bach2010 = mean(bach2010_share),
+    bach2018 = mean(bach2018_share),
+    dbach = (sum(bach2018) - sum(bach2010))/sum(bach2010),
+    broadband_sub = mean(broadband_sub),
+    broadband = sum(f477_maxad_downup_2018dec_25_3_popsum)/sum(pop2018_sum),
+    young_firm = sum(firm_10yr)/sum(lodes_all_jobs_2017),
+    prop = (sum(prop_emp_2018)-sum(prop_emp_2007))/sum(prop_emp_2007),
+    amenity = mean(amenity)
+  )
+
+
 write.csv(recovery_compare, "recession_recovery_compare.csv")
 write.csv(recovery_compare_nometro, "recession_recovery_compare_nometro.csv")
 
 write.csv(recovery_compare_alt, "recession_recovery_compare_alt.csv")
 write.csv(recovery_compare_alt_nometro, "recession_recovery_compare_nometro_alt.csv")
+
+write.csv(recovery_compare_black, "recession_recovery_high_black_share.csv")
+write.csv(recovery_compare_black_metro, "recovery_compare_black_metro.csv")
